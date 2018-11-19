09c0dc3fbc683509d4fa769c36eb4750cf7b06ba
Brian Nickila <brian.nickila@gmail.com>
2018-11-19 15:33:26 -0600

    All of the basics are up and running.

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $(".heading").on("click", function() {
+        $(this).parent().toggleClass("col-md-12");
+        //$(this).html("");
+        $(this).parent().siblings().toggleClass("hide");
+        
+    });
+    $(".titleHeader").on("click", function() {
+        $(".section").removeClass("col-md-12 hide");
+        $(".onClick").addClass("hide");
+        $(".onLoad").removeClass("hide");
+    })
+    $("#portHeader").on("click", function() {
+        $(this).siblings().toggleClass("hide");
+    });
+    $("#aboutHeader").on("click", function() {
+        $(this).siblings().toggleClass("hide");
+    });
+    
+
+});
